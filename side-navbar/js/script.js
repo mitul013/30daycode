656416197fbc135e54f9e4e64706ea74d6ccb723
Mitul Adroja <mitul.adroja13@gmail.com>
2020-07-01 23:50:41 +0530

    add gsap js for animation

--- side-navbar/js/script.js
+++ side-navbar/js/script.js
@@ -5,9 +5,24 @@ function scrollAppear(){
 	var screenposition = window.innerHeight/1.2;
 	
 	if(screenposition > textposition){
-		console.log('xx');
 		text[i].classList.add('text-appear');
+		console.log("zz");
 	}
+	}
+	
+	var image = document.getElementsByClassName("image");
+	
+	for(var i=0;i<image.length;i++){
+		var imageposition = image[i].getBoundingClientRect().top;
+		var screenposition = window.innerHeight/2;
+			console.log(imageposition);
+		if(imageposition < screenposition){
+		if(i%2){
+		gsap.fromTo(image[i], {x: 100}, {x: 0,opacity:1, duration: 1.5});}
+		else{
+		gsap.fromTo(image[i], {x: -100}, {x: 0,opacity:1 duration: 1.5})}
+			gsap.fromTo(image[i], {scale: 1.2}, {scale: 1, duration: 1});
+		}
 	}
 }
 scrollAppear();
